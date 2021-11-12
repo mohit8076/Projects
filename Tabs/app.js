e386433309dfd2f7efcf382d930d7cd715b74d17
mohit8076 <mynkchauhan1234@gmail.com>
2021-11-12 16:30:02 +0530

    day -6

--- Tabs/app.js
+++ Tabs/app.js
@@ -4,12 +4,12 @@ const articles = document.querySelectorAll(".content");
 about.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
   if (id) {
-    // remove selected from other buttons
+    
     btns.forEach(function (btn) {
       btn.classList.remove("active");
     });
     e.target.classList.add("active");
-    // hide other articles
+   
     articles.forEach(function (article) {
       article.classList.remove("active");
     });
